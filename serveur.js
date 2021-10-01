5ac4122f63e2adb73f6a2148f5a67fab8eb70bbe
Yewou <you@example.com>
2021-10-01 16:39:22 +0000

    fir commit

--- serveur.js
+++ serveur.js
@@ -1,11 +1,15 @@
-const express = require("express");
-const path = require("path");
+//Install express server
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/tektal'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/tektal'));
 
-app.get('/*', function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/tektal/index.html'));
-})
-app.listen(process.env.PORT);
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/tektal/'}),
+);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
